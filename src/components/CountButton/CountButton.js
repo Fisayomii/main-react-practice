40897763291dd74ccaac18fae96ee6ee44b76807
Fisayomi <estherjoshua2002@gmail.com>
2023-02-20 16:45:55 +0000

    commit

---
+++ src/components/CountButton/CountButton.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react"
+import "./CountButton.css"
+
+function CountButton (props) {
+   const [currentCount, setCurrentCount] = useState(0)
+    
+   
+   const handleClick = function(){
+        setCurrentCount(currentCount + props.incrementBy)
+    }
+
+    const buttonStyles = {
+        background: props.buttonColor,
+    }
+    return (
+
+        <div>
+            <button style={buttonStyles}
+            onClick={handleClick}> + {props.incrementBy}
+            </button>
+            <div className="count-display">{currentCount}</div>
+        </div>
+    )
+}
+
+export default CountButton
